091701af2e996644da9327103d6b645e0678b2ed
diatambe <diatambe1120@gmail.com>
2026-04-06 09:29:30 +0530

    Add files via upload

---
+++ tempbasicoperations.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Calculator
+{
+private:
+    T num1, num2;
+
+public:
+    Calculator(T n1, T n2)
+    {
+        num1 = n1;
+        num2 = n2;
+    }
+
+    void displayResult()
+    {
+        cout << "Numbers are: " << num1 << " and " << num2 << "." << endl;
+        cout << "Addition is: " << add() << endl;
+        cout << "Subtraction is: " << subtract() << endl;
+        cout << "Product is: " << multiply() << endl;
+
+        if (num2 != 0)
+            cout << "Division is: " << divide() << endl;
+        else
+            cout << "Division by zero is not allowed." << endl;
+    }
+
+    T add() { return num1 + num2; }
+    T subtract() { return num1 - num2; }
+    T multiply() { return num1 * num2; }
+    T divide() { return num1 / num2; }
+};
+
+int main()
+{
+    Calculator<int> c1(3, 6);
+    Calculator<float> c2(3.2, 6.7);
+
+    cout << "c1 add: " << c1.add() << endl;
+    cout << "c2 multiply: " << c2.multiply() << endl;
+
+    c1.displayResult();
+
+    return 0;
+}
